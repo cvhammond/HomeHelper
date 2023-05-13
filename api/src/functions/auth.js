9efb55e73499237357989d04bf14e48fa563e9b6
Claire V. Hammond <cvhammond@mailbox.org>
2023-05-12 23:44:49 -0700

    test https-only false

--- api/src/functions/auth.js
+++ api/src/functions/auth.js
@@ -153,7 +153,8 @@ export const handler = async (event, context) => {
       HttpOnly: true,
       Path: '/',
       SameSite: 'Strict',
-      Secure: process.env.NODE_ENV !== 'development',
+      //Secure: process.env.NODE_ENV !== 'development',
+      Secure: false,
 
       // If you need to allow other domains (besides the api side) access to
       // the dbAuth session cookie:
